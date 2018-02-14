e60c4722e66542b4b9e773805c5baa6bc2d01c6a
Derek Liu <derek.liu@mediatek.com>
2018-02-14 17:17:08 +0800

    docs(lec8-lec10): explain syntax

--- code/helloworld/main.go
+++ code/helloworld/main.go
@@ -1,7 +1,9 @@
-package main
+package main // declare this code is in the package called `main`
+// `main` package is a special package that is _executable_
 
-import "fmt"
+import "fmt" // get access to std lib `fmt`
 
 func main() {
 	fmt.Println("Hi there!")
+	// method of a package should starts with capitalized letter
 }
