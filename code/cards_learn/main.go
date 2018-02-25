f6a0569c78aa3e0090a408968de5097ced728bcc
Te-Chi Liu <liuderchi@gmail.com>
2018-02-25 20:56:49 +0800

    note(lec 16): slice syntax: declaration, append, range over a slice for-loop

--- code/cards_learn/main.go
+++ code/cards_learn/main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
+	cards := []string{"Five of Diamonds", newCard()} // declare a slice of strings
+	cards = append(cards, "Six of Spades")           // append an element to a slice
 
-	fmt.Println(card)
+	for i, card := range cards { // range over a slice NOTE: use :=
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
