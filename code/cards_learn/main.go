2f5010eb7283349a798abf937ac95ec00232c8b4
Te-Chi Liu <liuderchi@gmail.com>
2018-02-25 20:42:54 +0800

    note(lec14): var declaration and assign initial value syntax

--- code/cards_learn/main.go
+++ code/cards_learn/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// var msg string = "hello"   // go would suggest use shorthand :=
+	card := "Ace of Spades"
+
+	card = "Five of Dimonds" // reassignment
+
+	fmt.Println(card)
 }
