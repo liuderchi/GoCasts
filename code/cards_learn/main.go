a8ba3a9377d1dba9e3f2882c6003ef7948b5781d
Te-Chi Liu <liuderchi@gmail.com>
2018-02-25 20:48:58 +0800

    note(lec 15): declare a func newCard() with return type

--- code/cards_learn/main.go
+++ code/cards_learn/main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// var msg string = "hello"   // go would suggest use shorthand :=
-	card := "Ace of Spades"
-
-	card = "Five of Dimonds" // reassignment
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Ace of Spades"
 }
