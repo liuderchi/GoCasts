0d705e6915e904504a6411a8d359f662b0e288e9
Te-Chi Liu <liuderchi@gmail.com>
2018-02-26 18:04:37 +0800

    note(lec 38): declare an object with a struct

--- code/structs_learn/main.go
+++ code/structs_learn/main.go
@@ -1,10 +1,19 @@
 package main
 
+import (
+	"fmt"
+)
+
 type person struct { // a custom type
 	firstName string
 	lastName  string
 }
 
 func main() {
+	// alex := new person{"Alex", "Anderson"}  // method one: struct property order matters
 
+	alex := person{firstName: "Alex", lastName: "Anderson"}
+
+	fmt.Println(alex.firstName)
+	fmt.Println(alex.lastName)
 }
